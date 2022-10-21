075a1bbdc3e4ca31090333722124969ac54c7c9b
HadrienBoyer <hadrienboyer@gmail.com>
2022-10-21 06:13:50 +0200

    pwa up

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,25 +7,25 @@ Contact: hello@omiklo.com
 File: Main Js File
 */
 
-function initServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    // Register a service worker hosted at the root of the
-    // site using the default scope.
-    navigator.serviceWorker.register('/sw.js').then(
-      (registration) => {
-        console.log('Service worker registration succeeded:', registration)
-      },
-        /*catch*/ (error) => {
-          console.log('Service worker registration failed:', error)
-      }
-    )
-  } else {
-    console.log('Service workers are not supported.')
-  }
-}
-
 (function () {
   "use strict";
+
+  function initServiceWorker() {
+    if ('serviceWorker' in navigator) {
+      // Register a service worker hosted at the root of the
+      // site using the default scope.
+      navigator.serviceWorker.register('/sw.js').then(
+        (registration) => {
+          console.log('Service worker registration succeeded:', registration)
+        },
+          /*catch*/ (error) => {
+            console.log('Service worker registration failed:', error)
+        }
+      )
+    } else {
+      console.log('Service workers are not supported.')
+    }
+  }
 
   function initComponents() {
     var tooltipTriggerList = [].slice.call(
